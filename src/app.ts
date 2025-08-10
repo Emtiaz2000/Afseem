cec97e9c9a4078b42f2f6e947c06871ead31a880
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2025-08-11 00:47:02 +0600

    Update app.ts
    I have added ejs package and set-up ejs for frontend

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,8 @@
-import express, { Application, Request, Response } from 'express';
+import express, { Application, Request, Response,static as static_ } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import expressLayouts from 'express-ejs-layouts';
+import {dirname,join} from 'path';
 // import notFound from './app/middlewares/notFound';
 // import globalErrorHandler from './app/middlewares/globalErrorHandler';
 // import router from './app/routes';
@@ -9,14 +11,25 @@ const app: Application = express();
 
 //parsers
 app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(static_(join(__dirname,'public')))
 app.use(cors());
 app.use(cookieParser());
+
+//view engine
+app.set('view engine','ejs');
+app.use(expressLayouts)
+app.set('views',join(__dirname,'views'))//get views directory
+app.set('layout','layouts/layout')
 
 // application routes
 // app.use('/api', router);
 
 const getAController = (req: Request, res: Response) => {
-  res.send('Hello, E-Commerce TypeScript (^_^)');
+   //console.log(__dirname)
+   //res.send('Hello, E-Commerce TypeScript (^_^)');
+ 
+ res.render('pages/home',{welcomedata:"Hello, E-Commerce TypeScript (^_^)"})
 };
 
 app.get('/', getAController);
