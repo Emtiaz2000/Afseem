29fe3f7ebbb990ebbd18e976bcf148d34ad5fcb4
Emtiaz2000 <emon200002@gmail.com>
2025-09-24 01:22:17 +0600

    add google map

--- src/app/controlers/removebgFunction.js
+++ src/app/controlers/removebgFunction.js
@@ -4,8 +4,8 @@ import { spawn } from 'child_process';
 // Helper: run Python background removal
 export const runPython = (inputPath, outputPath)=>{
   return new Promise((resolve, reject) => {
-    const pythonProcess = spawn("./venv/bin/python", ["./src/app/pybgremove/bgremove.py", inputPath, outputPath]);
-    //const pythonProcess = spawn("python", ["./src/app/pybgremove/bgremove.py", inputPath, outputPath]);
+    //const pythonProcess = spawn("./venv/bin/python", ["./src/app/pybgremove/bgremove.py", inputPath, outputPath]);
+    const pythonProcess = spawn("python", ["./src/app/pybgremove/bgremove.py", inputPath, outputPath]);
 
     pythonProcess.stdout.on("data", (data) => {
       resolve(data.toString().trim());
