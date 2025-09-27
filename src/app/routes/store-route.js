cefa5052c05e120ab3f4199a215f9e1b628f3db4
Emtiaz2000 <emon200002@gmail.com>
2025-09-27 15:37:02 +0600

    google language making load more faster

--- src/app/routes/store-route.js
+++ src/app/routes/store-route.js
@@ -22,7 +22,6 @@ import {runPython,removeBackground,downloadImage} from '../controlers/removebgFu
 import pLimit from "p-limit";
 import ExcelJS  from 'exceljs'
 import fs from 'fs';
-import XLSX from 'xlsx';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
